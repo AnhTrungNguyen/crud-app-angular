ee08f074c867b4c2076bbb7ee31062687f7d66f4
AnhTrungNguyen <103439199+AnhTrungNguyen@users.noreply.github.com>
2023-07-27 15:18:02 +0700

    Update url api

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -9,18 +9,18 @@ export class EmployeeService {
   constructor(private _http: HttpClient) {}
 
   addEmployee(data: any): Observable<any> {
-    return this._http.post('http://localhost:3000/employees', data);
+    return this._http.post('https://crud-app-six-gamma.vercel.app/employees', data);
   }
 
   updateEmployee(id: number, data: any): Observable<any> {
-    return this._http.put(`http://localhost:3000/employees/${id}`, data);
+    return this._http.put(`https://crud-app-six-gamma.vercel.app/${id}`, data);
   }
 
   getEmployeeList(): Observable<any> {
-    return this._http.get('http://localhost:3000/employees');
+    return this._http.get('https://crud-app-six-gamma.vercel.app');
   }
 
   deleteEmployee(id: number): Observable<any> {
-    return this._http.delete(`http://localhost:3000/employees/${id}`);
+    return this._http.delete(`https://crud-app-six-gamma.vercel.app/${id}`);
   }
 }
